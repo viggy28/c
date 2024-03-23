5e4714ebc84d0d2f3045eeeaa11b5b2fb359b8a6
viggy28 <villan.lampard.epl@gmail.com>
2024-03-22 17:56:21 -0700

    update: nicer type syntax for function pointers

--- func-ptr/main.c
+++ func-ptr/main.c
@@ -5,14 +5,22 @@
 // which takes two integer arguments and returns an integer
 typedef int (*op)(int, int);
 
-// creates a new type called "op" which is a function pointer
+// creates a new type called "opp" which is a function pointer
 // which takes two integer arguments and returns an integer
 typedef int *(*opp)(int, int);
+
+// create a new type called "opplain" which is a function
+// this syntax make it easier to understand that the type is a function
+typedef int opplain(int, int);
 
 // process processes two integers and returns an integer
 int process(int a, int b, op operation) { return operation(a, b); }
 
-int *processp(int a, int b, opp operation) { return operation(a, b); }
+int *processp(int a, int b, opp operation) {
+  // operation is of type opp which is a function pointer which returns an
+  // integer pointer
+  return operation(a, b);
+}
 
 int mul(int a, int b) { return (a * b); }
 
@@ -31,7 +39,7 @@ int main() {
   printf("processed value is %d \n", output);
   output = process(4, 8, &add);
   printf("processed value is %d \n", output);
-  void *a[3];
+  void *a[4];
   a[0] = &add;
   output = process(4, 8, a[0]);
   printf("processed value is %d \n", output);
@@ -41,5 +49,7 @@ int main() {
   a[2] = &addp;
   int *outputp = processp(4, 10, a[2]);
   printf("processed value is %d \n", *outputp);
+  opplain *oppl = &add;
+  output = process(4, 8, oppl);
   return 0;
 }
